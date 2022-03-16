c8af08caf18bd0008acd6f98fe36679654648d9d
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-03-16 15:13:20 +0900

    BaseComponentにログアウトを移動

---
+++ components/BaseComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-row>
+      <v-col offset-md="6">
+        <span> ようこそ {{ $store.state.user.displayName }} さん</span>
+        <v-btn @click="$store.dispatch('logout')">Logout</v-btn>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseComponent'
+}
+</script>
+
+<style>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,8 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      Index Page
       <login-form v-if="!$store.getters.isAuthenticate" />
-      <v-btn v-else @click="$store.dispatch('logout')">Logout</v-btn>
+      <base-component v-else />
     </v-col>
   </v-row>
 </template>
