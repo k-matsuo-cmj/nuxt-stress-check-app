04bdb5e5423ad0991c95fe6232c6eebfa7ee58ef
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-03-16 17:50:47 +0900

    Windowコンポーネント追加

--- components/BaseComponent.vue
+++ components/BaseComponent.vue
@@ -6,12 +6,30 @@
         <v-btn @click="$store.dispatch('logout')">Logout</v-btn>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col>
+        <question-window v-if="onCheck" />
+        <div v-else>
+          <p>あなたの職場におけるストレスレベルを測定します。質問は全部で57問です。</p>
+          <p>この検査は、労働安全衛生法に基づくものです。</p>
+          <br>
+          <strong>それではストレスチェックを始めましょう！</strong>
+          <br>
+          <v-btn large color="primary" @click="onCheck=true">開始</v-btn>
+        </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'BaseComponent'
+  name: 'BaseComponent',
+  data() {
+    return {
+      onCheck: false
+    }
+  }
 }
 </script>
 
---
+++ components/QuestionWindow.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-card>
+    <v-window v-model="index">
+      <v-window-item v-for="n in length" :key="n">
+        <div>{{ `${n} / ${length} `}}</div>
+        Question!
+      </v-window-item>
+    </v-window>
+    <v-card-actions class="justify-space-between">
+      <v-btn text @click="prev"><v-icon>mdi-chevron-left</v-icon></v-btn>
+      <v-spacer />
+      <v-btn text @click="next"><v-icon>mdi-chevron-right</v-icon></v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'QuestionWindow',
+  data() {
+    return {
+      index: 0,
+      length: 57
+    }
+  },
+  methods: {
+    prev () {
+      if (this.index > 0) this.index--
+    },
+    next () {
+      if (this.index < this.length - 1) this.index++
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
