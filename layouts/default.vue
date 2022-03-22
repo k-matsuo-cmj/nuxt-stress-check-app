226e8468e58415606960131609d74ffa838642db
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-03-22 15:21:42 +0900

    ダークテーマ切替

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,18 +3,21 @@
     <v-app-bar fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-menu v-if="$store.getters.isAuthenticate">
+      <v-menu v-if="$store.getters.isAuthenticate" :close-on-content-click="false">
         <template #activator="{ on, attr }">
           <v-btn icon v-bind="attr" v-on="on">
             <v-icon>mdi-menu</v-icon>
           </v-btn>    
         </template>
         <v-list>
+          <v-list-item>
+            <v-switch v-model="$vuetify.theme.dark" label="ダークテーマ" />
+          </v-list-item>
           <v-list-item @click="$store.dispatch('logout')">
             <v-list-item-icon>
               <v-icon>mdi-logout</v-icon>
             </v-list-item-icon>
-            <v-list-item-content>Logout</v-list-item-content>
+            <v-list-item-content>ログアウト</v-list-item-content>
           </v-list-item>
         </v-list>
       </v-menu>
